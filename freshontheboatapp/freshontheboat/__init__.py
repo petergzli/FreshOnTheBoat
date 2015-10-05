542a06200fa0adf150819a8a3014aeef8e8ef62d
Peter Li <petergzli@gmail.com>
2015-10-04 20:44:32 -0700

    __init_py

--- freshontheboatapp/freshontheboat/__init__.py
+++ freshontheboatapp/freshontheboat/__init__.py
@@ -3,7 +3,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/freshontheboat'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://dcpaqcaqfgsknw:VL5J4ews_-CDvpZi06rEYImuXQ@ec2-54-197-241-24.compute-1.amazonaws.com:5432/df93a0pc1b0ioa'
 app.config['SECRET_KEY'] = 'super-secret'
 app.config['SQLALCHEMY_POOL_RECYCLE'] = 299
 app.config['SQLALCHEMY_POOL_TIMEOUT'] = 20
