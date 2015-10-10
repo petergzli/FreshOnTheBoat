f88db87797405f29340ce5eab38485beec937e5a
Peter Li <petergzli@gmail.com>
2015-10-10 09:16:06 -0700

    user has liked logic added to the get request for forum profiles

--- freshontheboattests/testGetForumProfiles.py
+++ freshontheboattests/testGetForumProfiles.py
@@ -2,6 +2,6 @@
 import requests
 
 url = 'http://127.0.0.1:5000/forum/getresults/'
-parameters = {'latitude': 34.068699, 'longitude': -118.445557}
+parameters = {'latitude': 34.068699, 'longitude': -118.445557, 'user_id': 1}
 HTTPresponse = requests.get(url, params  = parameters) 
 print HTTPresponse.json()
