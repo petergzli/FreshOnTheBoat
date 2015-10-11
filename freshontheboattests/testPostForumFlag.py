7c678397a04b9b2062d0bf1e3fe16858ba4f6e21
Peter Li <petergzli@gmail.com>
2015-10-11 13:31:10 -0700

    added the flag feature

--- freshontheboattests/testPostForumFlag.py
+++ freshontheboattests/testPostForumFlag.py
@@ -4,6 +4,6 @@ import requests
 url = 'http://127.0.0.1:5000/forum/flag/'
 username = "callmemaybe"
 password = "baby"
-parameters = {'forum_profile_flagged_id': 2}
+parameters = {'forum_profile_flagged_id': 1}
 HTTPresponse = requests.post(url, data = parameters, auth=(username,password))
 print HTTPresponse.json()
